43bb3d9f025659f23155ac60d91b8983bfa834b8
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-08-02 14:21:38 +0300

    Working on NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import Profile from './pages/profile/Profile';
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  // return <RouterProvider router={router}></RouterProvider>;
+  return <Profile />;
 }
 
 export default App;
