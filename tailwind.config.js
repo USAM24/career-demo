fab26bdb1e69be5d83953fe6466a51990f24ee6c
Mohamed Tarek <117052100+3tared@users.noreply.github.com>
2024-08-03 07:58:51 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,26 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+        primaryText: 'var(--primary-text-color)',
+        secondaryText: 'var(--secondary-text-color)',
+        tertiaryText: 'var(--tertiary-text-color)',
+        neutralWhite: 'var(--neutral-white-color)',
+        neutralBlack: 'var(--neutral-black-color)',
+      },
+      backgroundColor: {
+        'teal-Nav': '#F6FAFB',
+        'green-button': '#20B486',
+      },
+      textColor: {
+        'green-text': '#56BC80',
+      },
+    },
+  },
+  plugins: [],
+};
