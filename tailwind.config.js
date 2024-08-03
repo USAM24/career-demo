27a30e2984b98a86f71bbfc687d93bc44ee7c05e
Mohamed Tarek <117052100+3tared@users.noreply.github.com>
2024-08-03 07:57:02 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {backgroundColor: {
-      'teal-Nav': '#F6FAFB',
-      'green-button': '#20B486'
-    },
-textColor: {
-    'green-text': '#56BC80'}
-}},
+    extend: {}
   plugins: [],
 };
